db773dfb5ac087c81b92b4a5094a7ab82cffacc9
Scott Gardner <scottgardner23@gmail.com>
2024-02-27 23:04:55 +1100

    I wanted to use some vim tmux bindings, so I need to remap my current harpoon config

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,7 +4,10 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<C-i>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-o>", function() ui.nav_file(2) end)
+-- Trying to set up tmux at the moment and I really want to be able to switch between
+-- panes with <C-hjkl>, but I can't think of a better four letters than I currently have.
+--
+-- vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+-- vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
