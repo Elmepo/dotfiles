c8902c419956dadd76b5ef1b3d6c0febbc735a6b
Scott Gardner <scott@hanasoft.com>
2024-06-04 09:48:50 +1000

    Disabling the background to allow my terminal background to show (pretty pictures)

--- nvim/lua/scott/lazy/rose-pine.lua
+++ nvim/lua/scott/lazy/rose-pine.lua
@@ -9,6 +9,9 @@ return {
       lazy = false,
       name = 'rose-pine',
       config = function()
+          require('rose-pine').setup({
+              disable_background = true,
+          })
           vim.cmd('colorscheme rose-pine-moon')
           ColorScreen()
       end,
